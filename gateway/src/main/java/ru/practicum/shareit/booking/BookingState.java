021e962639fb0f8a485b59c9c2aa4ed40d3490c5
igor-shablevskii <shablevich@yandex.ru>
2022-11-04 17:56:12 +0100

    fix: Delete models from gateway

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingState.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingState.java
@@ -2,13 +2,4 @@ package ru.practicum.shareit.booking;
 
 public enum BookingState {
     ALL, CURRENT, PAST, FUTURE, WAITING, REJECTED;
-
-    static BookingState from(String state) {
-        for (BookingState value : BookingState.values()) {
-            if (value.name().equals(state)) {
-                return value;
-            }
-        }
-        return null;
-    }
 }
